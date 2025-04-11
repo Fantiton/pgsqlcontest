af0c5cb4a93579bbba72adbc9cf26b040fbb5b2f
Fantiton <filipcelejewski@wp.pl>
2025-04-11 09:39:56 +0200

    nadganianie

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const express = require('express');
 const pg = require('pg');
-
+const app = express();
 const client = new pg.Client({
     user: 'user2',
     host: '192.168.0.207',
@@ -8,6 +9,63 @@ const client = new pg.Client({
     port: 5432,
 });
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.get('/students/:student_id/grades/', async (req, res) => {
+    const studentId = req.params.student_id;
+    console.log("StudentId: " + studentId);
+    const query = "SELECT * FROM grades WHERE student_id = " + studentId;
+    const result = await client.query(query);
+    console.log("Query Result:");
+    console.table(result.rows);
+    res.send(result.rows);
+});
+
+app.post('/students/:student_id/grades/', async (req, res) => {
+    const studentId = parseInt(req.params.student_id);
+    console.log("Student ID:", studentId);
+    console.log("Request Body:", req.body);
+
+    if(studentId === null || !Number.isInteger(studentId) || !isFinite(studentId)) {
+        res.status(400).send('Invalid student ID');
+        return;
+    }
+
+    if(req.body.course_id === null || !Number.isInteger(req.body.course_id) || !isFinite(req.body.course_id)) {
+        res.status(400).send('Invalid course ID');
+        return;
+    }
+
+    if(req.body.grade === null || !Number.isInteger(req.body.grade) || !isFinite(req.body.grade)){
+        res.status(400).send('Invalid grade');
+        return;
+    }
+
+    const query = "INSERT INTO grades (student_id, course_id, grade) VALUES ($1, $2, $3)";
+    const values = [studentId, req.body.course_id, req.body.grade];
+    await client.query(query, values)
+    res.end();
+    });
+
+    app.listen(3000, async () => {
+        await client.connect();
+        console.log('Connected to the database');
+        console.log('Server is running on port 3000');
+
+    });
+
+    app.on('close', async () => {
+        await client.end();
+        console.log('Disconnected from the database');
+    })
+
+
+/*
 const getFromDb = async () => {
 
     const res = await client.query('SELECT * FROM grades');
@@ -30,3 +88,4 @@ const main = async ()=> {
 }
 
 main();
+*/
